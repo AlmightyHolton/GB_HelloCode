70a173548260ed95e08aeb7bf1c16421f5a5ad82
Anton <almighty.holton@gmail.com>
2023-03-06 16:24:06 +0800

    add Example 013 Recursion Algoritm

---
+++ Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿                   //
+string[,] table = new string[2, 5];
+// String.Empty
+// table[0,0]; table[0,1] table[0,2] table[0,4]
+// table[1,0]; table[1,1] table[1,2] table[1,4]
+
+
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
